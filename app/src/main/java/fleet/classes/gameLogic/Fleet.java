c0d511c54d625b72fba37466056659a63dad6d88
Radu <conner.ferguson1@marist.edu>
2015-09-27 17:37:54 +0900

    Added fleet class structure

--- app/src/main/java/fleet/classes/gameLogic/Fleet.java
+++ app/src/main/java/fleet/classes/gameLogic/Fleet.java
@@ -1,10 +1,22 @@
 package fleet.classes.gameLogic;
 
+import java.lang.reflect.Array;
 import java.util.EnumSet;
 
 /**
  * Created by Radu on 9/27/2015.
  */
 public class Fleet {
-    protected EnumSet shipTypes;
+    protected String fleetPath;
+    protected String FleetName;
+    protected Ship carrier;
+    protected Ship[] battleships = new Ship[4];
+    protected Ship[] cruisers = new Ship[4];
+    protected Ship[] destroyers = new Ship[4];
+
+    public Fleet(String fleetPath){
+
+
+    }
+
 }
