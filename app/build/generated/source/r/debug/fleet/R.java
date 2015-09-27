f6e844de97de880f90f83ec2656243bc267d45fc
OperationMech <operationmech@gmail.com>
2015-09-27 07:04:34 -0400

    last few

--- app/build/generated/source/r/debug/fleet/R.java
+++ app/build/generated/source/r/debug/fleet/R.java
@@ -16,12 +16,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int options_button_down=0x7f020001;
-        public static final int options_button_up=0x7f020002;
-        public static final int play_button_down=0x7f020003;
-        public static final int play_button_up=0x7f020004;
-        public static final int title_background=0x7f020005;
-        public static final int title_graphic=0x7f020006;
+        public static final int mute_button_down=0x7f020001;
+        public static final int mute_button_up=0x7f020002;
+        public static final int options_button_down=0x7f020003;
+        public static final int options_button_up=0x7f020004;
+        public static final int play_button_down=0x7f020005;
+        public static final int play_button_up=0x7f020006;
+        public static final int title_background=0x7f020007;
+        public static final int title_graphic=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f090000;
