019a60cbaadeba740973fe15aae4512683f9679c
Radu <conner.ferguson1@marist.edu>
2015-09-27 19:39:12 +0900

    May the force be with you

--- app/src/main/java/fleet/view/TitleView.java
+++ app/src/main/java/fleet/view/TitleView.java
@@ -20,11 +20,15 @@ public class TitleView extends View {
     private Bitmap playButtonDown;
     private Bitmap optionsButtonUp;
     private Bitmap optionsButtonDown;
+    private Bitmap muteButtonUp;
+    private Bitmap muteButtonDown;
     private Bitmap titleBackground;
     private int screenW;
     private int screenH;
     private boolean playButtonPressed;
     private boolean optionsButtonPressed;
+    private boolean muteButtonPressed;
+    private boolean mute;
     private Context myContext;
     private MediaPlayer mp;
 
@@ -38,15 +42,18 @@ public class TitleView extends View {
         playButtonDown = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_down);
         optionsButtonUp = BitmapFactory.decodeResource(getResources(), R.drawable.options_button_up);
         optionsButtonDown = BitmapFactory.decodeResource(getResources(), R.drawable.options_button_down);
-        mp = MediaPlayer.create(context, R.raw.title_bgm);
+        muteButtonUp = BitmapFactory.decodeResource(getResources(), R.drawable.mute_button_up);
+        muteButtonDown = BitmapFactory.decodeResource(getResources(), R.drawable.mute_button_down);
+        mp = MediaPlayer.create(context, fleet.R.raw.title_bgm);
         mp.setLooping(true);
         mp.start();
+        mute = false;
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasWindowFocus) {
         super.onWindowFocusChanged(hasWindowFocus);
-        if (hasWindowFocus) {
+        if (hasWindowFocus && mute == false) {
             mp.start();
         } else {
             mp.pause();
@@ -56,15 +63,35 @@ public class TitleView extends View {
     public void onDraw(Canvas canvas) {
         canvas.drawBitmap(titleBackground, 0, 0, null);
         canvas.drawBitmap(titleGraphic, 0, 0, null);
+        if (mute == true) {
+            canvas.drawBitmap(muteButtonDown, (screenW - muteButtonUp.getWidth()), 0, null);
+        } else {
+            canvas.drawBitmap(muteButtonUp, (screenW - muteButtonUp.getWidth()), 0, null);
+        }
+
         if (playButtonPressed) {
             canvas.drawBitmap(playButtonDown, (screenW - playButtonDown.getWidth()) / 2, (int) (screenH * 0.7), null);
         } else {
             canvas.drawBitmap(playButtonUp, (screenW - playButtonUp.getWidth()) / 2, (int) (screenH * 0.7), null);
         }
+
         if (optionsButtonPressed) {
             canvas.drawBitmap(optionsButtonDown, (screenW - optionsButtonDown.getWidth()) / 2, (int) (screenH * 0.5), null);
         } else {
             canvas.drawBitmap(optionsButtonUp, (screenW - optionsButtonUp.getWidth()) / 2, (int) (screenH * 0.5), null);
+        }
+
+        if (muteButtonPressed) {
+            if (mute == true) {
+                canvas.drawBitmap(muteButtonUp, (screenW - muteButtonUp.getWidth()), 0, null);
+                mp.start();
+                mute = false;
+            } else {
+                canvas.drawBitmap(muteButtonDown, (screenW - muteButtonDown.getWidth()), 0, null);
+                mp.pause();
+                mute = true;
+            }
+
         }
     }
 
@@ -102,6 +129,14 @@ public class TitleView extends View {
                     break;
                 }
 
+                if (x > (screenW - muteButtonUp.getWidth()) &&
+                        x < ((screenW - muteButtonUp.getWidth())) + muteButtonUp.getWidth() &&
+                        y > (int) 0 &&
+                        y < (int) +muteButtonUp.getHeight()) {
+                    muteButtonPressed = true;
+                    break;
+                }
+
             case MotionEvent.ACTION_MOVE:
 
                 break;
@@ -111,6 +146,7 @@ public class TitleView extends View {
 
                     myContext.startActivity(gameIntent);
                 }
+                muteButtonPressed = false;
                 optionsButtonPressed = false;
                 playButtonPressed = false;
                 break;
