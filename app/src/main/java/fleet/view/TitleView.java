9c0ccb0c7076a76e385193c54b6def0bc98230d7
Radu <conner.ferguson1@marist.edu>
2015-09-27 14:07:19 +0900

    Test

--- app/src/main/java/fleet/view/TitleView.java
+++ app/src/main/java/fleet/view/TitleView.java
@@ -1,33 +1,43 @@
 package fleet.view;
 
-import fleet.R;
-import fleet.activity.GameActivity;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.view.MotionEvent;
-import android.view.View;
+	public TitleView(Context context) {
+		super(context);import fleet.R;
+		import fleet.activity.GameActivity;
+		import android.content.Context;
+		import android.content.Intent;
+		import android.graphics.Bitmap;
+		import android.graphics.BitmapFactory;
+		import android.graphics.Canvas;
+		import android.media.MediaPlayer;
+		import android.view.MotionEvent;
+		import android.view.View;
 
 public class TitleView extends View {
 	private Bitmap titleGraphic;
 	private Bitmap playButtonUp;
 	private Bitmap playButtonDown;
+	private Bitmap optionsButtonUp;
+	private  Bitmap optionsButtonDown;
 	private Bitmap titleBackground;
 	private int screenW;
 	private int screenH;
 	private boolean playButtonPressed;
+	private boolean optionsButtonPressed;
 	private Context myContext;
-	
-	public TitleView(Context context) {
-		super(context);
-		myContext = context;
+	private MediaPlayer mp;
+
+
+	myContext = context;
 		titleBackground = BitmapFactory.decodeResource(getResources(),R.drawable.title_background);
-		titleGraphic = BitmapFactory.decodeResource(getResources(), fleet.R.drawable.title_graphic);
-		titleGraphic = BitmapFactory.decodeResource(getResources(), R.drawable.title_graphic);
-		playButtonUp = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_up);
+	titleGraphic = BitmapFactory.decodeResource(getResources(), fleet.R.drawable.title_graphic);
+	titleGraphic = BitmapFactory.decodeResource(getResources(), R.drawable.title_graphic);
+			playButtonUp = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_up);
 		playButtonDown = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_down);
+		optionsButtonUp = BitmapFactory.decodeResource(getResources(),R.drawable.options_button_up);
+		optionsButtonDown = BitmapFactory.decodeResource(getResources(),R.drawable.options_button_down);
+		mp = MediaPlayer.create(context, R.raw.title_bgm);
+		mp.setLooping(true);
+		mp.start();
 	}
 	
 	public void onDraw(Canvas canvas) {
@@ -37,6 +47,11 @@ public class TitleView extends View {
 			canvas.drawBitmap(playButtonDown, (screenW-playButtonDown.getWidth())/2, (int)(screenH*0.7), null);
 		} else {
 			canvas.drawBitmap(playButtonUp, (screenW-playButtonUp.getWidth())/2, (int)(screenH*0.7), null);			
+		}
+		if (optionsButtonPressed) {
+			canvas.drawBitmap(optionsButtonDown, (screenW-optionsButtonDown.getWidth())/2, (int)(screenH*0.5), null);
+		} else {
+			canvas.drawBitmap(optionsButtonUp, (screenW-optionsButtonUp.getWidth())/2, (int)(screenH*0.5), null);
 		}
 	}
 	
@@ -63,9 +78,17 @@ public class TitleView extends View {
 		        		y > (int)(screenH*0.7) &&
 		        		y < (int)(screenH*0.7) + playButtonUp.getHeight()) {
 		        		playButtonPressed = true;
+					 	break;
 		        	}
-				break;
-				
+
+			if (x > (screenW-optionsButtonUp.getWidth())/2 &&
+					x < ((screenW-optionsButtonUp.getWidth())/2) + optionsButtonUp.getWidth() &&
+					y > (int)(screenH*0.5) &&
+					y < (int)(screenH*0.5) + optionsButtonUp.getHeight()) {
+					optionsButtonPressed = true;
+					break;
+				}
+
 			case MotionEvent.ACTION_MOVE:
 				
 				break;
@@ -75,6 +98,7 @@ public class TitleView extends View {
 		        	
 		        	myContext.startActivity(gameIntent);	        		
 	        	}
+				optionsButtonPressed = false;
 	        	playButtonPressed = false;
 				break;
 		}
